7eb15d6e3458dd14d78046c598e39f52ced8a854
Angelica Donios <angelicadonios@gmail.com>
2026-03-23 21:32:15 +0800

    first commit

---
+++ frontend/client-jp/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+
+const LandingPage = () => {
+  
+  return (
+    <div className="min-h-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ frontend/client-jp/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+  return (
+    <div>
+        <Outlet />
+        
+    </div>
+    
+  )
+}
+
+export default ProtectedRoute
